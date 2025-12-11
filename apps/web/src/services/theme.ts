c80c96cabb764115754860bdc56f1c82dfb8b5bc
Akshay Priyadarshi <akshayp1904@outlook.com>
2025-12-11 15:40:34 +0530

    fix errors

--- apps/web/src/services/theme.ts
+++ apps/web/src/services/theme.ts
@@ -1,14 +1,14 @@
 import { createServerFn } from "@tanstack/react-start";
 import { getCookie, setCookie } from "@tanstack/react-start/server";
 
-import { Theme, themeSchema } from "@/schemas";
+import { UserTheme, userThemeSchema } from "@/schemas";
 
 const storageKey = "_preferred-theme";
 
 export const getThemeServerFn = createServerFn().handler(
-	async () => (getCookie(storageKey) || "system") as Theme,
+	async () => (getCookie(storageKey) || "system") as UserTheme,
 );
 
 export const setThemeServerFn = createServerFn({ method: "POST" })
-	.inputValidator(themeSchema)
+	.inputValidator(userThemeSchema)
 	.handler(async ({ data }) => setCookie(storageKey, data));
